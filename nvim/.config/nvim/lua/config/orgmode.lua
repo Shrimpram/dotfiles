3f510acbb3e4e428563449232fad857631cb264a
Shreeram Modi <67710369+Shrimpram@users.noreply.github.com>
2021-07-27 11:55:53 -0700

    NVIM: Remove orgmode templates

--- nvim/.config/nvim/lua/config/orgmode.lua
+++ nvim/.config/nvim/lua/config/orgmode.lua
@@ -1,11 +1,6 @@
 require('orgmode').setup{
   org_agenda_files = '~/Sync/org/*',
   org_default_notes_file = '~/Sync/org/inbox.org',
-  org_agenda_templates = {
-    t = { description = 'Quick Task', template = '* TODO %?\n %u' },
-    d = { description = 'Task With Deadline', template = '* TODO %?\n DEADLINE: %t\n %u' },
-    a = { description = 'Appointment', template = '* %?\n SCHEDULED %t\n %u' }
-  },
   org_deadline_warning_days = 2,
   org_agenda_skip_scheduled_if_done = true,
   org_agenda_skip_deadline_if_done = true,
