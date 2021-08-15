836495a3b4498d5d7b5f29bad901b2a2c919d92f
Shreeram Modi <67710369+Shrimpram@users.noreply.github.com>
2021-08-14 18:30:59 -0700

    NVIM: Don't skip org scheduled or deadline if done

--- nvim/.config/nvim/lua/config/orgmode.lua
+++ nvim/.config/nvim/lua/config/orgmode.lua
@@ -2,7 +2,5 @@ require('orgmode').setup{
   org_agenda_files = '~/Sync/org/*',
   org_default_notes_file = '~/Sync/org/inbox.org',
   org_deadline_warning_days = 2,
-  org_agenda_skip_scheduled_if_done = true,
-  org_agenda_skip_deadline_if_done = true,
   org_use_tag_inheritance = true,
 }
