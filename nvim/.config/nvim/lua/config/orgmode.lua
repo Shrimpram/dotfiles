4ad29d453749798e6085fe24cf57c91e70d0b807
Shreeram Modi <67710369+Shrimpram@users.noreply.github.com>
2021-08-20 22:09:56 -0700

    NVIM: Update org files location

--- nvim/.config/nvim/lua/config/orgmode.lua
+++ nvim/.config/nvim/lua/config/orgmode.lua
@@ -1,5 +1,5 @@
 require('orgmode').setup{
-  org_agenda_files = '~/Sync/org/*',
+  org_agenda_files = '~/Dropbox/org/*',
   org_default_notes_file = '~/Sync/org/inbox.org',
   org_deadline_warning_days = 2,
   org_use_tag_inheritance = true,
